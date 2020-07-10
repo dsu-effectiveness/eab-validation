536b008a750fd658b0b0786541d926027ce2e71a
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-07-10 09:14:44 -0600

    cleaned up code

--- count_by_residency.sql
+++ count_by_residency.sql
@@ -1,52 +1,49 @@
 --Residency
---Student Level Query
-SELECT stl.student_id,
-       CASE
-           WHEN stl.residency_code IN ('C', 'S') THEN 'R'
-           WHEN stl.residency_code IN ('0', 'H') THEN 'N'
-           ELSE residency_code
-           END AS s_rpt_res,
-       stl.full_time_part_time_code,
-       stl.term_id,
-       stl.is_registered
-  FROM student_term_level stl
-       LEFT JOIN term t
-                 ON stl.term_id = t.term_id
-       LEFT JOIN student s
-                 ON s.student_id = stl.student_id
- WHERE stl.level_id <> 'GR'
-   AND t.term_id = '201940'
-   AND stl.student_id = '00409316';
 
---Students03 summary query logic
-SELECT count(stl.student_id),
+/*************************************************************************************
+ Compared to STUDENTS03
+
+  --Count by Residency
+SELECT count(dsc_pidm),
+       s_rpt_res
+  FROM students03
+ WHERE dsc_term_code = '20194E'
+   AND s_level <> 'GG'
+ GROUP BY s_rpt_res
+ ORDER BY s_rpt_res;
+ ************************************************************************************/
+
+-- Summary query logic
+SELECT count(student_id),
        CASE
-           WHEN stl.residency_code IN ('C', 'S') THEN 'R'
-           WHEN stl.residency_code IN ('0', 'H') THEN 'N'
+           WHEN residency_code IN ('C', 'S') THEN 'R'
+           WHEN residency_code IN ('0', 'H') THEN 'N'
            ELSE residency_code
            END AS s_rpt_res
-  FROM student_term_level stl
-       LEFT JOIN term t
-                 ON stl.term_id = t.term_id
-       LEFT JOIN student s
-                 ON s.student_id = stl.student_id
- WHERE stl.level_id <> 'GR'
-   AND t.term_id = '201940'
+  FROM student_term_level
+ WHERE level_id <> 'GR'
+   AND term_id = '201940'
    --AND s.is_registered = 'Y'
-   AND stl.full_time_part_time_code in ('F', 'P')
+   AND full_time_part_time_code IN ('F', 'P')
  GROUP BY CASE
-              WHEN stl.residency_code IN ('C', 'S') THEN 'R'
-              WHEN stl.residency_code IN ('0', 'H') THEN 'N'
+              WHEN residency_code IN ('C', 'S') THEN 'R'
+              WHEN residency_code IN ('0', 'H') THEN 'N'
               ELSE residency_code
               END;
 
-SELECT is_in_state_resident
-  FROM student;
+--Student Level Query
+SELECT student_id,
+       CASE
+           WHEN residency_code IN ('C', 'S') THEN 'R'
+           WHEN residency_code IN ('0', 'H') THEN 'N'
+           ELSE residency_code
+           END AS s_rpt_res,
+       full_time_part_time_code,
+       term_id,
+       is_registered
+  FROM student_term_level stl
+ WHERE level_id <> 'GR'
+   AND full_time_part_time_code IN ('F', 'P')
+   AND term_id = '201940';
 
-/*
-             case
-                when sgbstdn_resd_code in ('R', 'N', 'A', 'M', 'G') then sgbstdn_resd_code
-                when sgbstdn_resd_code in ('C', 'S') then 'R'
-                when sgbstdn_resd_code in ('0', 'H') then 'N'
-            end as s_regent_res
- */
+
