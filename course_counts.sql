9045eb7f7ce8a1740468ba73d3900088ff75e27d
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-09-02 10:18:13 -0600

    initial sql

--- course_counts.sql
+++ course_counts.sql
@@ -1,15 +1,22 @@
 SELECT DISTINCT
        sis_system_id AS pidm,
        is_registered,
-       primary_major_campus_id,
        term_id,
        a.*
-  FROM student_term_level a
+  FROM student_section a
  WHERE term_id = '202030'
-   --AND is_registered = 'Y'
-   AND sis_system_id = '2007'
+   AND is_registered = 'Y'
+   AND sis_system_id = '52845'
 ORDER BY 1;
 
-SELECT *
-FROM student_section
-WHERE sis_system_id = '2007';
+SELECT DISTINCT
+       sis_system_id AS pidm,
+       is_registered,
+       term_id,
+       a.*
+  FROM student_section a
+ WHERE term_id = '202030'
+   AND sis_system_id = '52845'
+ORDER BY 1;
+
+
