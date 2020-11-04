91696ceb09ecfac8c23c1d48f2b04809f386de92
Justin Pfeifer <Justin.Pfeifer@dixie.edu>
2020-11-04 15:00:21 -0700

    removed file

--- graduate_counts.sql
+++ graduate_counts.sql
@@ -1,3 +1,22 @@
+--SELECT COUNT(DISTINCT sis_system_id)
+SELECT DISTINCT sis_system_id
+FROM student_degree a
+WHERE a.graduated_term_id = '202020'
+AND degree_status_code = 'AW';
+
+SELECT sis_system_id
+FROM student_degree_program_application a
+WHERE a.graduated_term_id = '202020'
+AND degree_status_code = 'AW';
+
+SELECT *
+FROM student_degree a
+WHERE sis_system_id = '76749';
+
+SELECT *
+FROM student_degree_program_application
+WHERE sis_system_id = '76749';
+
 SELECT DISTINCT
        sis_system_id AS pidm,
        is_registered,
